1a4a608d4021d1835809c4e89e36cae30bcddf03
ManojNRaikar <manojnraikar123@gmail.com>
2025-10-19 00:02:11 +0530

    Fix constructor parameter formatting in BasePage for consistency

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,7 +1,7 @@
 import { Page } from "@playwright/test";
 export class BasePage{
     protected readonly page:Page
-    constructor( page:Page){
+    constructor(page:Page){
         this.page=page
     }
 }
