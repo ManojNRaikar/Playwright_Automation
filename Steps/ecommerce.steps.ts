19be27eb4be5300238f1cc02d850c21b36f8f647
ManojNRaikar <manojnraikar123@gmail.com>
2025-10-18 23:28:54 +0530

    Add hooks.ts file and update ecommerce.steps.ts for future step definitions

--- Steps/ecommerce.steps.ts
+++ Steps/ecommerce.steps.ts
@@ -8,6 +8,7 @@ Given('I login to the demo store', async function() {
 });
 When('I enter valid credentials', async function () {
   // await loginPage.login("testuser_1", "Test@12345");
+  
 });
 
 Then('I should be logged in successfully', async function () {
