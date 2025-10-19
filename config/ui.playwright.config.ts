0f391c2be6a2e94462528e8eac451f89f05c9e98
ManojNRaikar <manojnraikar123@gmail.com>
2025-10-19 22:39:32 +0530

    feat: add Playwright configuration and BDD setup for testing

--- config/ui.playwright.config.ts
+++ config/ui.playwright.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const testDir = defineBddConfig({
   features: 'features/*.feature',
-  steps: ['Steps/**/*.ts','Hooks/hooks.ts'],
+  steps: ['Steps/*.{ts,js}','Hooks/hooks.ts'],
 
 });
 
@@ -49,6 +49,30 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+
+  //   {
+  //     name: 'firefox',
+  //     use: { ...devices['Desktop Firefox'] },
+  //   },
+
+  //   {
+  //     name: 'webkit',
+  //     use: { ...devices['Desktop Safari'] },
+  //   },
+
+  //   /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
   //   /* Test against branded browsers. */
     {
