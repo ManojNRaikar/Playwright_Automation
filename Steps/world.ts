7edc2702878ba3a71aa66fd83a48583cddda91dc
ManojNRaikar <manojnraikar123@gmail.com>
2025-10-19 15:18:10 +0530

    refactor: remove unused HomePage reference from World class

--- Steps/world.ts
+++ Steps/world.ts
@@ -1,14 +1,11 @@
 import { Page, TestInfo } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
-import { HomePage } from "../pages/HomePage"
 // import * as Pages from "../pages/index"
 
 export class World{
     login:LoginPage;
-    homepage:HomePage;
 
     constructor(public page:Page, public testInfo: TestInfo){
         this.login = new LoginPage(page);
-        this.homepage = new HomePage(page)
     }
 }
